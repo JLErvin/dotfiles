6a79f9d07b70c432ac1834ea441a74ab2f16d2d9
Joshua Ervin <ervin.josh17@gmail.com>
2018-03-08 23:14:39 -0800

    Removed fish, zsh is better

--- fish/.config/config.fish
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env fish
-
-set PATH /usr/local/bin /usr/sbin $PATH
-set PATH /usr/local/bin /home/void/.scripts $PATH
-
-function fish_prompt
-    set_color green
-    echo (dirs) '> '
-end
